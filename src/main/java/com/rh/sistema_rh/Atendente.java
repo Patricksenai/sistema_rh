00cb9cd49233be36d3f23a5aa658373475059ede
Patrick Vinicius Coelho Lima <PET0152991@aluno.firjansenaisesi.com.br>
2023-09-06 12:01:59 -0300

    alteração classe atendente

--- src/main/java/com/rh/sistema_rh/Atendente.java
+++ src/main/java/com/rh/sistema_rh/Atendente.java
@@ -1,5 +1,13 @@
 package com.rh.sistema_rh;
 
+import com.rh.sistema_rh.enums.Salario;
+
 public class Atendente extends Pessoa {
+
+    Salario salario = Salario.Atendentesa;
+
+    public Atendente(int id, String nome, String cpf, int dataNascimento, String ctps) {
+        super(id, nome, cpf, dataNascimento, ctps);
+    }
     
 }
