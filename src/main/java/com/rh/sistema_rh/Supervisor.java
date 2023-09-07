a6fd0d6cc60817011b849ee9af8462c5f9605401
Patricksenai <petrick.lima@aluno.senai.br>
2023-09-07 14:43:55 -0300

    adição do salario e dos tributos na classe Supervisor

--- src/main/java/com/rh/sistema_rh/Supervisor.java
+++ src/main/java/com/rh/sistema_rh/Supervisor.java
@@ -2,12 +2,36 @@ package com.rh.sistema_rh;
 
 import com.rh.sistema_rh.enums.Salario;
 
-public class Supervisor extends Pessoa {
+public class Supervisor extends Funcionario {
+    Salario salario = Salario.Supervisorsa;
 
     Salario Supervisorsa;
 
-    public Supervisor(int id, String nome, String cpf, int dataNascimento, String ctps) {
-        super(id, nome, cpf, dataNascimento, ctps);
+    public Supervisor( String nome, String cpf, int dataNascimento, String ctps) {
+        super(nome, cpf, dataNascimento, ctps);
+    }
+
+    @Override
+    public double fgts(double salario) {
+        return salario * 0.08;
+      
+    }
+
+    @Override
+    public double inss(double salario) {
+        return salario * 0.01;
+    } 
+
+    @Override
+    public double decimo(double salario) {
+        return salario /12;
+    
+    }
+
+    @Override
+    public double férias(double salario) {
+        return salario * 0.08;
+     
     }
     
 }
