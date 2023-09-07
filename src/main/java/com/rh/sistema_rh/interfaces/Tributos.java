e58f5b2f41260eb4ffba2a32a480dc159ebd458b
Patricksenai <petrick.lima@aluno.senai.br>
2023-09-07 14:03:13 -0300

    altreração nome interface descontos para tributos

---
+++ src/main/java/com/rh/sistema_rh/interfaces/Tributos.java
@@ -0,0 +1,11 @@
+package com.rh.sistema_rh.interfaces;
+
+public interface Tributos {
+    void fgts(double salario);
+    void inss(double salario);
+    void decimo(double salario);
+    void férias(double salario);
+
+
+    
+}
