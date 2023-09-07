1bdb1bd58c97466d557000e74c56559e82bd92ba
Patricksenai <petrick.lima@aluno.senai.br>
2023-09-07 14:41:34 -0300

    adição do salario e dos tributos na classe gerente

--- src/main/java/com/rh/sistema_rh/Gerente.java
+++ src/main/java/com/rh/sistema_rh/Gerente.java
@@ -2,12 +2,39 @@ package com.rh.sistema_rh;
 
 import com.rh.sistema_rh.enums.Salario;
 
-public class Gerente extends Pessoa {
+public class Gerente extends Funcionario {
+
+    Salario salario = Salario.Gerentesa;
+
 
     Salario Gerentesa;
     
-    public Gerente(int id, String nome, String cpf, int dataNascimento, String ctps) {
-        super(id, nome, cpf, dataNascimento, ctps);
+    public Gerente( String nome, String cpf, int dataNascimento, String ctps) {
+        super( nome, cpf, dataNascimento, ctps);
+    }
+
+    @Override
+    public double fgts(double salario) {
+        return salario * 0.08;
+       
+      
+    }
+
+    @Override
+    public double inss(double salario) {
+        return salario * 0.01;
+    }
+
+    @Override
+    public double decimo(double salario) {
+        return salario / 12;
+    
+    }
+
+    @Override
+    public double férias(double salario) {
+        return salario * 0.08;
+   
     }
     
 }
